06a7240636767773eded5f449d86295089fb32f3
Vikas Chahar <misfityudi@gmail.com>
2025-02-27 23:48:31 +0530

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
-pub struct Stack<T>{
-    pub items: Vec<T>
+pub struct Stack<T> {
+    pub items: Vec<T>,
 }
 
-impl<T> Stack<T>{
-    pub fn new()->Self{
-        Self{
-            items: Vec::new()
-        }
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Self { items: Vec::new() }
     }
 
-    pub fn push(&mut self, value: T){
+    pub fn push(&mut self, value: T) {
         self.items.push(value);
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -23,15 +20,15 @@ mod tests {
 
     #[test]
     fn new() {
-       let stack = Stack::<i8>::new();
-       assert!(stack.items.is_empty());
+        let stack = Stack::<i8>::new();
+        assert!(stack.items.is_empty());
     }
 
     #[test]
-    fn push(){
+    fn push() {
         let mut stack = Stack::<i8>::new();
-        let value= i8::MAX;
-        
+        let value = i8::MAX;
+
         stack.push(value);
         assert_eq!(stack.items.len(), 1);
         assert_eq!(stack.items[0], i8::MAX);
