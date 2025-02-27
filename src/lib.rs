8d2afff2a3210e035f9cd42c95d5ce8093ea8284
Vikas Chahar <misfityudi@gmail.com>
2025-02-27 23:44:43 +0530

    add push method, and tests for it

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,32 @@ impl<T> Stack<T>{
             items: Vec::new()
         }
     }
+
+    pub fn push(&mut self, value: T){
+        self.items.push(value);
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use std::i8;
+
     use super::*;
 
     #[test]
     fn new() {
        let stack = Stack::<i8>::new();
        assert!(stack.items.is_empty());
+    }
+
+    #[test]
+    fn push(){
+        let mut stack = Stack::<i8>::new();
+        let value= i8::MAX;
+        
+        stack.push(value);
+        assert_eq!(stack.items.len(), 1);
+        assert_eq!(stack.items[0], i8::MAX);
     }
 }
